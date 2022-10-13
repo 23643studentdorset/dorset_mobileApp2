1a005f8aa3c97d5cf3f34fc69a2da388894efbf5
23643@student.dorset-college.ie <23643@student.dorset-college.ie>
2022-10-13 14:14:58 +0100

    Update DataBaseActivity.kt
    .

--- app/src/main/java/com/saravanabalagi/dorset_mobile_app_2/DataBaseActivity.kt
+++ app/src/main/java/com/saravanabalagi/dorset_mobile_app_2/DataBaseActivity.kt
@@ -3,81 +3,63 @@ package com.saravanabalagi.dorset_mobile_app_2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.room.*
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
 
 class DatabaseActivity : AppCompatActivity() {
+    val TAG = "DatabaseActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_database)
 
-        Thread {
-            val db = Room.databaseBuilder(
-                applicationContext,
-                AppDatabase::class.java, "database-maps"
-            ).build()
+        val db = Firebase.firestore
+        val usersCollection = db.collection("users")
 
-            val userDao = db.userDao()
+        // Create a new user with a first and last name
+//        val user1 = User(1,"Ada1", "Lovelace", 1915)
+//        val user2 = User(2,"Ada2", "Lovelace", 1915)
+//        val user3 = User(3,"Ada3", "Lovelace", 1915)
+//        val user4 = User(4,"Ada4", "Lovelace", 1915)
+//        val user5 = User(5,"Ada5", "Lovelace", 1915)
+//
+//        arrayOf(user1, user2, user3, user4, user5).map { user ->
+//            val name = "${user.fname} ${user.lname}"
+//            usersCollection.document(user.id.toString())
+//                .set(user)
+//                .addOnSuccessListener {
+//                    Log.d(TAG, "User $name added with ID: ${user.id}")
+//                }
+//                .addOnFailureListener { e ->
+//                    Log.w(TAG, "Error adding user $name", e)
+//                }
+//        }
 
-            // Insert new user
-            userDao.insert(User(1, "Some name 1"))
-            userDao.insert(User(2, "Some name 2"))
+        // Update and delete ops
+//        usersCollection.document("1").update("last", "Updated lastname")
+//        usersCollection.document("5").delete()
+//            .addOnSuccessListener {
+//                Log.i(TAG, "Delete user 5 succeeded")
+//            }
+//            .addOnFailureListener {
+//            Log.i(TAG, "Delete user 5 failed")
+//        }
 
-            val usersToInsert = listOf(
-                User(3, "Some name 3"),
-                User(4, "Some name 4"),
-                User(5, "Some name 5"),
-                User(6, "Some name 6"),
-            )
-            userDao.insertList(usersToInsert)
-
-            // Update user
-            val userToUpdate = User(3, "Updated name 3")
-            userDao.update(userToUpdate)
-
-            // Delete user
-            val userToDelete = User(2, null)
-            userDao.delete(userToDelete)
-
-            // retrieve all users
-            val users: List<User> = userDao.getAll()
-            users.map {
-                Log.i("DatabaseAct", it.toString())
-            }
-        }.start()
-
+        // Get
+        val userId = 3
+        usersCollection.document(userId.toString()).get().addOnSuccessListener {
+            val user = it.toObject<User>()
+            if (user != null)
+                Log.i(TAG, user.fname)
+            else Log.e(TAG, "User $userId is null")
+        }
     }
 }
 
-@Database(entities = [User::class], version = 1)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun userDao(): UserDao
-}
+class User(
+    var id: Int = -1,
+    var fname: String = "",
+    var lname: String = "",
+    var born: Int = -1,
+)
 
-@Entity
-data class User(val uid: Int, val uname: String?) {
-    @PrimaryKey var id: Int = uid
-    var name: String? = uname
-
-    override fun toString(): String {
-        return "$name ($id)"
-    }
-}
-
-@Dao
-interface UserDao {
-
-    @Query("SELECT * FROM User")
-    fun getAll(): List<User>
-
-    @Insert
-    fun insert(user: User)
-
-    @Insert
-    fun insertList(users: List<User>)
-
-    @Update
-    fun update(user: User)
-
-    @Delete
-    fun delete(user: User)
-}
